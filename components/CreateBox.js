f99e39a1c25428f97f7d0061a18ccfcbef504e31
jigsawye <jigsaw.ye@gmail.com>
2015-08-22 21:46:58 +0800

    Add createBox for message

---
+++ components/CreateBox.js
@@ -0,0 +1,59 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class CreateBox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      target: false
+    };
+
+  }
+
+  targetForm() {
+    const target = ! this.state.target;
+
+    this.setState({
+      target
+    });
+  }
+
+  onSubmit(e) {
+
+    e.preventDefault();
+  }
+
+  render() {
+    if (this.state.target) {
+      return (
+        <form className="createbox" onSubmit={this.onSubmit}>
+          <p><b>Leave a Message...</b></p>
+          <div className="form-group row">
+            <label htmlFor="title" className="col-sm-2 form-control-label">Title</label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control form-control-sm" id="title" />
+            </div>
+          </div>
+          <div className="form-group row">
+            <label htmlFor="content" className="col-sm-2 form-control-label">Content</label>
+            <div className="col-sm-10">
+              <textarea className="form-control form-control-sm" id="content" />
+            </div>
+          </div>
+          <div className="form-group row">
+            <div className="col-sm-offset-7 col-sm-2">
+              <button className="btn btn-primary btn-sm">Send</button>
+            </div>
+            <div className="col-sm-2">
+              <button type="reset" onClick={::this.targetForm} className="btn btn-danger btn-sm">Cancel</button>
+            </div>
+          </div>
+        </form>
+      );
+    } else {
+      return (
+        <button onClick={::this.targetForm} className="btn btn-primary btn-block btn-sm">Create a New Message</button>
+      );
+    }
+  }
+}
